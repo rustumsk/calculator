8bd060b02ed95dea5087c8187bc777f168e68c7a
Rustum <rustumarellano@gmail.com>
2024-01-16 22:28:02 +0800

    already working, need some polishing

--- script.js
+++ script.js
@@ -1,27 +1,80 @@
 let num1;
 let num2;
 let operator;
+let count = 0;
+let display = document.querySelector(".text")
+
+let buttons = document.querySelectorAll(".button");
+let deleteButton = document.querySelector(".delete")
+    .addEventListener("click",() => {
+        display.textContent = display.textContent.slice(0,-1);
+        count !== 0? count--: count;
+    })
+let clearButton = document.querySelector(".clear")
+    .addEventListener("click",() => {
+        let lastChild = display.lastChild;
+        display.removeChild(lastChild);
+        count = 0;
+    })
+buttons.forEach((button) =>{
+    button.addEventListener("click",() =>{
+        if(button.textContent === "+" || button.textContent === "-"
+        || button.textContent === "*" || button.textContent === "/"
+        || button.textContent === "^"){
+            count++;
+        }
+        if (count === 2){
+            operator = checkOperator(display.textContent);
+            let nums = display.textContent.split(operator);
+            display.textContent = operateNum(operator,parseInt(nums[0]),parseInt(nums[1]));
+            count = 0;
+        }
+        else{
+            display.textContent = display.textContent + button.textContent;
+        }
+    })
+})
 
 function operateNum(operator,num1,num2){
     switch (operator){
         case "+":
-            addNum(num1,num2);
-            break;
+            return addNum(num1,num2);
         case "-":
-            subNum(num1,num2);
-            break;
+            return subNum(num1,num2);         
         case "*":
-            multNum(num1,num2);
-            break;
+            return multNum(num1,num2);   
         case "/":
-            divNum(num1,num2);
-            break;
+            return divNum(num1,num2); 
         case "^":
-            powNum(num1,num2);
-            break;
+            return powNum(num1,num2);   
         default:
             console.log("Hello");
     }
+}
+
+function checkOperator(string){
+    let split = string.split("");
+    let value = "";
+    split.forEach((char) => {
+        switch (char){
+            case "+":
+                value = char;
+                break;
+            case "-":
+                value = char;    
+                break;  
+            case "*":
+                value = char;
+                break;  
+            case "/":
+                value = char;
+                break;
+            case "^":
+                value = char;
+                break;
+        }
+    })
+    return value;
 }
 function addNum(num1,num2){
     return num1 + num2;
